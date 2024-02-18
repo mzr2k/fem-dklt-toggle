a8a43ae98d1a6737629671038225cb7eeddd6e98
Marium Rehman <100401684+mzr2k@users.noreply.github.com>
2024-02-18 23:04:46 +0500

    fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // Use dart-sass for @use
-sass.compiler = require('dart-sass');
+//sass.compiler = require('dart-sass');
 
 // Sass Task
 function scssTask() {
